ed504cd574e1b7cf69f41978e04af7b517c41d63
Edith Thaba <writersherry5@gmail.com>
2023-12-28 10:35:47 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    let winningYear = record.find(function(entry) {
+      return entry.result === 'W';
+    });
+  
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
+  
